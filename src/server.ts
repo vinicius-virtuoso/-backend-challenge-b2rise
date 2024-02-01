8630c77b84d4af136343aabd18fea80115fa446e
Vinicius Virtuoso <viniciusvirtuoso1902@gmail.com>
2024-02-01 10:37:18 -0300

    chore: base do projeto e inicialização do servidor

---
+++ src/server.ts
@@ -0,0 +1,37 @@
+import "express-async-errors";
+import "dotenv/config";
+import express, { Express } from "express";
+import { errorHandler } from "@/app/error";
+
+export class Server {
+  private app: Express;
+  private baseUrl: string;
+
+  constructor() {
+    this.app = express();
+    this.baseUrl = "/api/v1";
+  }
+
+  public middlewares() {
+    this.app.use(express.json());
+  }
+
+  public erroServer() {
+    this.app.use(errorHandler);
+  }
+
+  public routes() {
+    console.log(this.baseUrl);
+    this.app.use(`${this.baseUrl}/users`);
+  }
+
+  public init(port: number = 3000) {
+    this.middlewares();
+    this.routes();
+    this.erroServer();
+
+    this.app.listen(port, () => {
+      console.log("listening on port " + port);
+    });
+  }
+}
