c9ae58041cdeb1c79bef029268d20783e33eb292
Vinicius Virtuoso <viniciusvirtuoso1902@gmail.com>
2024-02-01 15:07:32 -0300

    chore: adicionado novo contrato findByUsernameOrEmail

--- src/app/repositories/users-repository.ts
+++ src/app/repositories/users-repository.ts
@@ -1,9 +1,14 @@
 import { Users } from "../entities/users-entity";
-import { IUserResponse } from "../interfaces/users-interfaces";
+import {
+  IUserResponse,
+  IUserWithPasswordResponse,
+} from "../interfaces/users-interfaces";
 
 export abstract class UsersRepository {
   abstract create(data: Users): Promise<IUserResponse>;
-  abstract findByUsername(username: string): Promise<IUserResponse | null>;
+  abstract findByUsername(
+    username: string
+  ): Promise<IUserWithPasswordResponse | null>;
   abstract findByUsernameOrEmail(
     email: string,
     username: string
