e0cbc5345f9d1ef53f17fbe3ea42433654df26d5
Vinicius Virtuoso <viniciusvirtuoso1902@gmail.com>
2024-02-01 20:12:41 -0300

    feat: prevenção de admins duplicados

--- src/app/middlewares/admins-middleware/admin-duplicated-exists.ts
+++ src/app/middlewares/admins-middleware/admin-duplicated-exists.ts
@@ -1,11 +1,15 @@
 import { AppError } from "@/app/error";
 import { IAdminRequest } from "@/app/interfaces/admins-interfaces";
-import { UsersRepository } from "@/app/repositories/users-repository";
+import { AdminsRepository } from "@/app/repositories/admins-repository";
 import { NextFunction, Request, Response } from "express";
 
-export const adminDuplicatedExists = (usersRepository: UsersRepository) => {
+export const adminDuplicatedExists = (usersRepository: AdminsRepository) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     const { username } = req.body as IAdminRequest;
+
+    if (!req.user.is_admin) {
+      throw new AppError("You not have permission to access", 403);
+    }
 
     if (username) {
       const user = await usersRepository.findByUsername(username);
@@ -14,6 +18,7 @@ export const adminDuplicatedExists = (usersRepository: UsersRepository) => {
         throw new AppError("User already exists.", 409);
       }
     }
+
     return next();
   };
 };
