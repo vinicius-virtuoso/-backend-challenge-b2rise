b2d4a5aef902e1b943662d40be9a2afa9cdcbd54
Vinicius Virtuoso <viniciusvirtuoso1902@gmail.com>
2024-02-02 12:45:33 -0300

    feat: criação de metodos de atualização. leitura e deleção de produtos

--- src/app/services/products/products-get-one.ts
+++ src/app/services/products/products-get-one.ts
@@ -1,7 +1,7 @@
 import { ProductsRepository } from "@/app/repositories/products-repository";
 import { PrismaProductsRepository } from "@/infra/database/repositories/prisma-products-repository";
 
-export const productsGetOneService = async (
+export const productGetOneService = async (
   productId: string,
   productsRepository: ProductsRepository = new PrismaProductsRepository()
 ) => {
--- src/infra/http/controllers/products-controller.ts
+++ src/infra/http/controllers/products-controller.ts
@@ -1,31 +1,49 @@
 import { ProductsRepository } from "@/app/repositories/products-repository";
 import { productCreateService } from "@/app/services/products/products-create";
+import { productDeleteService } from "@/app/services/products/products-delete";
+import { productsGetAllService } from "@/app/services/products/products-get-all";
+import { productGetOneService } from "@/app/services/products/products-get-one";
+import { productUpdateService } from "@/app/services/products/products-update";
 import { Request, Response } from "express";
 
 export class ProductsController {
-  repository: ProductsRepository;
+  productsRepository: ProductsRepository;
 
-  constructor(repository: ProductsRepository) {
-    this.repository = repository;
+  constructor(productsRepository: ProductsRepository) {
+    this.productsRepository = productsRepository;
   }
   async create(req: Request, res: Response) {
-    const product = await productCreateService(req.body, this.repository);
+    const product = await productCreateService(
+      req.body,
+      this.productsRepository
+    );
     return res.status(201).json(product);
   }
 
   async findOne(req: Request, res: Response) {
-    return res.status(200).json();
+    const product = await productGetOneService(
+      req.params.productId,
+      this.productsRepository
+    );
+    return res.status(200).json(product);
   }
 
   async findAll(req: Request, res: Response) {
-    return res.status(200).json();
+    const products = await productsGetAllService(this.productsRepository);
+    return res.status(200).json(products);
   }
 
   async update(req: Request, res: Response) {
-    return res.status(200).json();
+    const product = await productUpdateService(
+      req.params.productId,
+      req.body,
+      this.productsRepository
+    );
+    return res.status(200).json(product);
   }
 
   async delete(req: Request, res: Response) {
+    await productDeleteService(req.params.productId, this.productsRepository);
     return res.status(204).json();
   }
 }
