2663973561865c0d8b023ff6e83d215df1286566
Vinicius Virtuoso <viniciusvirtuoso1902@gmail.com>
2024-02-02 12:26:29 -0300

    feat: serviço de atualização de produto

---
+++ src/app/services/products/products-update.ts
@@ -0,0 +1,11 @@
+import { IProductRequest } from "@/app/interfaces/products-interfaces";
+import { ProductsRepository } from "@/app/repositories/products-repository";
+import { PrismaProductsRepository } from "@/infra/database/repositories/prisma-products-repository";
+
+export const userUpdateService = async (
+  productId: string,
+  data: Partial<IProductRequest>,
+  productsRepository: ProductsRepository = new PrismaProductsRepository()
+) => {
+  return await productsRepository.update(productId, data);
+};
