8426d83fd6dd665ee6c6aed4467164d6b6a3fe89
Vinicius Virtuoso <viniciusvirtuoso1902@gmail.com>
2024-02-02 10:14:44 -0300

    chore: contrato de repository de produtos

---
+++ src/app/repositories/products-repository.ts
@@ -0,0 +1,14 @@
+import { Products } from "@app/entities/products-entity";
+import { IProductResponse } from "../interfaces/products-interfaces";
+
+export abstract class ProductsRepository {
+  abstract create(data: Products): Promise<IProductResponse>;
+
+  abstract findById(id: string): Promise<IProductResponse | null>;
+
+  abstract getAll(): Promise<IProductResponse[]>;
+
+  abstract update(data: Partial<IProductResponse>): Promise<IProductResponse>;
+
+  abstract delete(id: string): Promise<void>;
+}
