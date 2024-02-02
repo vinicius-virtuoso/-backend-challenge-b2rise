9fa670e4cef1652ba7bb757a75c023a91af58e1b
Vinicius Virtuoso <viniciusvirtuoso1902@gmail.com>
2024-02-02 11:36:45 -0300

    fix: ajuste no middleware de verifição se o user é adiminstrador

--- src/app/middlewares/admins-middleware/is-admin.ts
+++ src/app/middlewares/admins-middleware/is-admin.ts
@@ -1,14 +1,16 @@
 import { AppError } from "@/app/error";
 import { NextFunction, Request, Response } from "express";
 
-export const isAdmin = () => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    const isAdmin = req.user.is_admin;
+export const isAdmin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const isAdmin = req.user.is_admin;
 
-    if (isAdmin === false) {
-      throw new AppError("You not have permission to access", 403);
-    }
+  if (isAdmin === false) {
+    throw new AppError("You not have permission to access", 403);
+  }
 
-    return next();
-  };
+  return next();
 };
