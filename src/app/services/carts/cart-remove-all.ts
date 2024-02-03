3a0c454611a18518d0480e7309dc137346147759
Vinicius Virtuoso <viniciusvirtuoso1902@gmail.com>
2024-02-02 21:41:23 -0300

    feat: serviço de remoção de todos os produtos do carrinho de compras

---
+++ src/app/services/carts/cart-remove-all.ts
@@ -0,0 +1,30 @@
+import { CartsItemsRepository } from "@/app/repositories/carts-items";
+import { CartsRepository } from "@/app/repositories/carts-repository";
+import { PrismaCartsItemsRepository } from "@/infra/database/repositories/prisma-carts-items-repository";
+import { PrismaCartsRepository } from "@/infra/database/repositories/prisma-carts-repository";
+
+export const cartRemoveAllService = async (
+  userId: string,
+  cartsRepository: CartsRepository = new PrismaCartsRepository(),
+  cartsItemsRepository: CartsItemsRepository = new PrismaCartsItemsRepository()
+) => {
+  let cart = await cartsRepository.get(userId);
+  let count = 0;
+  let total = 0;
+
+  if (cart) {
+    await cartsItemsRepository.delete(cart.id);
+    cart = await cartsRepository.get(userId);
+    if (cart) {
+      count = cart.products.reduce((acc, att) => {
+        return acc + att.quantity;
+      }, 0);
+
+      total = cart.products.reduce((acc, att) => {
+        return acc + att.product.price * att.quantity;
+      }, 0);
+
+      return await cartsRepository.update(cart.id, { count, total });
+    }
+  }
+};
