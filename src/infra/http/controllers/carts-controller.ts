f216462e92bb0d602f2a839f61ce2af4306735d5
Vinicius Virtuoso <viniciusvirtuoso1902@gmail.com>
2024-02-02 21:51:45 -0300

    feat: controller de rotas de criação, leitura, atualização carrinho de compras

---
+++ src/infra/http/controllers/carts-controller.ts
@@ -0,0 +1,69 @@
+import { CartsItemsRepository } from "@/app/repositories/carts-items";
+import { CartsRepository } from "@/app/repositories/carts-repository";
+import { ProductsRepository } from "@/app/repositories/products-repository";
+import { cartAddProduct } from "@/app/services/carts/cart-add-product";
+import { cartDecrementProductService } from "@/app/services/carts/cart-decrement-product";
+import { cartGetService } from "@/app/services/carts/cart-get";
+import { cartRemoveAllService } from "@/app/services/carts/cart-remove-all";
+import { cartRemoveProductService } from "@/app/services/carts/cart-remove-product";
+import { Request, Response } from "express";
+
+export class CartsController {
+  cartsRepository: CartsRepository;
+  productsRepository: ProductsRepository;
+  cartItemsRepository: CartsItemsRepository;
+
+  constructor(
+    cartsRepository: CartsRepository,
+    productsRepository: ProductsRepository,
+    cartItemsRepository: CartsItemsRepository
+  ) {
+    this.cartsRepository = cartsRepository;
+    this.productsRepository = productsRepository;
+    this.cartItemsRepository = cartItemsRepository;
+  }
+
+  async findOne(req: Request, res: Response) {
+    const cart = await cartGetService(req.user.id, this.cartsRepository);
+    return res.status(200).json(cart);
+  }
+
+  async addProduct(req: Request, res: Response) {
+    const cart = await cartAddProduct(
+      req.user.id,
+      req.params.productId,
+      this.cartsRepository,
+      this.productsRepository,
+      this.cartItemsRepository
+    );
+    return res.status(200).json(cart);
+  }
+
+  async subProduct(req: Request, res: Response) {
+    const cart = await cartDecrementProductService(
+      req.user.id,
+      req.params.productId,
+      this.cartsRepository
+    );
+    return res.status(200).json(cart);
+  }
+
+  async removeProduct(req: Request, res: Response) {
+    const cart = await cartRemoveProductService(
+      req.user.id,
+      req.params.productId,
+      this.cartsRepository,
+      this.cartItemsRepository
+    );
+    return res.status(200).json(cart);
+  }
+
+  async removeProductAll(req: Request, res: Response) {
+    const cart = await cartRemoveAllService(
+      req.user.id,
+      this.cartsRepository,
+      this.cartItemsRepository
+    );
+    return res.status(200).json(cart);
+  }
+}
