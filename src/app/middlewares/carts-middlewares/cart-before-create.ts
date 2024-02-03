37d81a412b06590284b59ee1fcc3ffb2d1fcd190
Vinicius Virtuoso <viniciusvirtuoso1902@gmail.com>
2024-02-02 21:43:41 -0300

    feat: serviço de criação automatica de carrinho de compras
    Rotas de carrinho de compras precisam de um carrinho existente para serem operadas.
    
    Verifica se o usuario já possui um carrinho de compras, caso não possua ele fará a criação automatica.

---
+++ src/app/middlewares/carts-middlewares/cart-before-create.ts
@@ -0,0 +1,18 @@
+import { CartsRepository } from "@/app/repositories/carts-repository";
+import { cartCreateService } from "@/app/services/carts/cart-create";
+import { NextFunction, Request, Response } from "express";
+
+export const cartBeforeCreate = (cartsRepository: CartsRepository) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const user_id = req.user.id;
+
+    const cart = await cartsRepository.get(user_id);
+
+    if (!cart) {
+      await cartCreateService({ userId: user_id }, cartsRepository);
+      return next();
+    }
+
+    return next();
+  };
+};
