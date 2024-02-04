01d4208e73801f41aef5ebabfffb3a3bf7c3c37c
Vinicius Virtuoso <viniciusvirtuoso1902@gmail.com>
2024-02-04 11:04:17 -0300

    feat: serviço de filtro de produtos por categoria

---
+++ src/app/services/products/products-by-category.ts
@@ -0,0 +1,27 @@
+import { ProductsRepository } from "@/app/repositories/products-repository";
+
+export const productsByCategory = async (
+  url: string,
+  category: string,
+  take: number,
+  page: number,
+  productsRepository: ProductsRepository
+) => {
+  const [products, total] = await productsRepository.findByCategory(category);
+  const pagesTotal = Math.ceil(total / take);
+
+  return {
+    page,
+    pagePrevious:
+      page - 1 <= 0
+        ? null
+        : `${process.env.BASE_URL_APP}${url}?page=${page - 1}&take=${take}`,
+
+    pageNext:
+      page + 1 > pagesTotal
+        ? null
+        : `${process.env.BASE_URL_APP}${url}?page=${page + 1}&take=${take}`,
+    count: total,
+    products,
+  };
+};
