caecf729151310bb8e397ecb9f80b711a36115fa
Vinicius Virtuoso <viniciusvirtuoso1902@gmail.com>
2024-02-04 10:50:19 -0300

    feat: filtragem,contagem e paginação de produtos

--- src/app/services/products/products-get-all.ts
+++ src/app/services/products/products-get-all.ts
@@ -4,13 +4,19 @@ export const productsGetAllService = async (
   url: string,
   page: number,
   take: number,
+  category: string,
+  title: string,
   min_price: number,
   max_price: number,
   productsRepository: ProductsRepository
 ) => {
+  const titleReplace = title && decodeURIComponent(title).replaceAll("-", " ");
+
   const [products, total] = await productsRepository.getAll(
     page,
     take,
+    category,
+    titleReplace,
     min_price,
     max_price
   );
@@ -22,11 +28,40 @@ export const productsGetAllService = async (
     pagePrevious:
       page - 1 <= 0
         ? null
-        : `${process.env.BASE_URL_APP}${url}?page=${page - 1}`,
+        : `${process.env.BASE_URL_APP}${url}?page=${page - 1}&take=${take}${
+            category !== "undefined" &&
+            category !== undefined &&
+            category !== null &&
+            category
+              ? `&category=${category}`
+              : ""
+          }${
+            title !== "undefined" &&
+            title !== undefined &&
+            title !== null &&
+            title
+              ? `&title=${title}`
+              : ""
+          }&min_price=${min_price}&max_price=${max_price}`,
+
     pageNext:
       page + 1 > pagesTotal
         ? null
-        : `${process.env.BASE_URL_APP}${url}?page=${page + 1}`,
+        : `${process.env.BASE_URL_APP}${url}?page=${page + 1}&take=${take}${
+            category !== "undefined" &&
+            category !== undefined &&
+            category !== null &&
+            category
+              ? `&category=${category}`
+              : ""
+          }${
+            title !== "undefined" &&
+            title !== undefined &&
+            title !== null &&
+            title
+              ? `&title=${title}`
+              : ""
+          }&min_price=${min_price}&max_price=${max_price}`,
     count: total,
     products,
   };
